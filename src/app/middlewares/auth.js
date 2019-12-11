b878be2c4f2313d7edd508e9f9cb839a9b3f98f4
Vinicius Felix <viniciusfelix.exe@gmail.com>
2019-12-11 19:56:29 -0300

    Authentication

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -4,32 +4,32 @@ const authConfig = require('../../config/auth.json');
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
   
-  // // Verifica se existe um token
-  // if(!authHeader)
-  //   return res.status(401).send({ error: "No token provided." });
+  // Verifica se existe um token
+  if(!authHeader)
+    return res.status(401).send({ error: "No token provided." });
 
-  // // Separando o bearer do hash
-  // const parts = authHeader.split(' ');
+  // Separando o bearer do hash
+  const parts = authHeader.split(' ');
 
-  // if(!parts.length === 2)
-  //   return res.status(401).send({ error: "Token error." });
+  if(!parts.length === 2)
+    return res.status(401).send({ error: "Token error." });
 
-  // const [ scheme, token ] = parts;
+  const [ scheme, token ] = parts;
 
-  // if(!/^Bearer$/i.test(scheme))
-  //   return res.status(401).send({ error: "Token malformatted." });
+  if(!/^Bearer$/i.test(scheme))
+    return res.status(401).send({ error: "Token malformatted." });
 
-  // jwt.verify(token, authConfig.secret, (err, decoded) => {
-  //   if(err)
-  //     return res.status(401).send({ error: "Invalid token." });
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if(err)
+      return res.status(401).send({ error: "Invalid token." });
 
-  //   req.userId = decoded.id;
-  //   req.name = decoded.name;
-  //   req.email = decoded.email;
+    req.userId = decoded.id;
+    req.name = decoded.name;
+    req.email = decoded.email;
     
-  //   return next();
-  // });
+    return next();
+  });
 
-  return next(); // remover
+  // return next(); // remover
 
 }
